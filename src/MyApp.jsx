a19a6353b4876d16c4b7648f95af7b055e5dea52
hhhhzs <1554652660@qq.com>
2022-06-30 11:32:20 +0800

    origin

---
+++ src/MyApp.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Avatar, ShellBar, ShellBarItem } from "@ui5/webcomponents-react";
+import "@ui5/webcomponents-icons/dist/add.js";
+import { Routes, Route, Navigate,useNavigate } from "react-router-dom";
+import { Home } from "./Home";
+import { Detail } from "./Detail";
+
+
+export function MyApp(){
+    const navigate = useNavigate();
+    const handleLogoClick = () => {
+      navigate("./");
+    };    
+  return (
+    <div>
+        <ShellBar
+          logo={<img src="https://raw.githubusercontent.com/sap-tutorials/Tutorials/master/tutorials/ui5-webcomponents-react-dashboard/reactLogo.png" alt=""/>}
+          profile={<Avatar><img src="https://raw.githubusercontent.com/sap-tutorials/Tutorials/master/tutorials/ui5-webcomponents-react-dashboard/profilePictureExample.png" alt=""/></Avatar>}
+          primaryTitle="My App"
+          onLogoClick={handleLogoClick}
+        >
+            <ShellBarItem icon="add" text="Add" />
+        </ShellBar>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/" element={<Navigate replace to="/home" />} />
+        </Routes>
+
+    </div>
+  );
+}
